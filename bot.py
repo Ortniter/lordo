26525720ffe48ff05cac9dd36e7352b0a3befb77
serho <hlavatskyi.serhii@steelkiwi.com>
2023-04-27 23:06:29 +0300

    Create set_db in bot

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import logging
 
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-from tortoise import run_async
+from tortoise import run_async, Tortoise
 
 import settings
 import handlers
+from palantir import Palantir
 from database.connect_db import connect_db
 
 logging.basicConfig(level=logging.INFO)
@@ -29,6 +30,16 @@ def main():
     application.run_polling()
 
 
+async def set_db():
+    await connect_db()
+    await Tortoise.generate_schemas()
+    palantir = Palantir(
+        token=settings.ONE_API_TOKEN,
+        base_url=settings.ONE_API_URL
+    )
+    await palantir.gather()
+
+
 if __name__ == '__main__':
-    run_async(connect_db())
+    run_async(set_db())
     main()
